855ee17e847495d6121a472c2149f5369120cafa
laury237 <micheleateutchia@gmail.com>
2021-02-04 09:28:50 +0100

    my first commit

---
+++ src/main/java/com/MasterMind/SendEmailfromContactForm/SendEmailfromContactFormApplication.java
@@ -0,0 +1,13 @@
+package com.MasterMind.SendEmailfromContactForm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendEmailfromContactFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendEmailfromContactFormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MasterMind/SendEmailfromContactForm/SendEmailfromContactFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MasterMind.SendEmailfromContactForm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendEmailfromContactFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
